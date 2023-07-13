6a81443666d70f23310f50b32f277f21eb2b3361
Ichikyube <ichikyube@gmail.com>
2023-07-14 04:18:59 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host:  process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      origin: ['*'],
+      cors: true
+    },
+    debug: false
+    // debug: 
+    // { 
+    //     request: ['*'],
+    //     log: ['*']
+    // }
+  });
+  server.route(routes);
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
